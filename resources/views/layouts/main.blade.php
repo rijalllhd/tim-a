7772d8902c48b2bf1a95e0ef07aa32bf2d2a8685
rijalllhd <ijal1445b@gmail.com>
2023-11-20 14:00:17 +0700

    nampilin users di sidebar

--- resources/views/layouts/main.blade.php
+++ resources/views/layouts/main.blade.php
@@ -70,8 +70,8 @@
                 <div id="collapseTwo" class="collapse" aria-labelledby="headingTwo" data-parent="#accordionSidebar">
                     <div class="bg-white py-2 collapse-inner rounded">
                         <h6 class="collapse-header">Custom Components:</h6>
-                        <a class="collapse-item {{($title === "Buttons") ? 'active' : ''}}" href="{{ route('formulir') }}">Data Pegawai</a>
-                        <a class="collapse-item" href="{{ route('cards') }}">Data Pengguna</a>
+                        <a class="collapse-item {{($title === 'Buttons') ? 'active' : ''}}" href="{{ route('formulir') }}">Data Pegawai</a>
+                        <a class="collapse-item {{($title === 'Buttons') ? 'active' : ''}}" href="{{ route('pengguna') }}">Data Pengguna</a>
                     </div>
                 </div>
             </li>
