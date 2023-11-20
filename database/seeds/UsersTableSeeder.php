a267e2a2ddf1090d8ec9db5178f1afbb3e16975f
rijalllhd <ijal1445b@gmail.com>
2023-11-20 14:36:11 +0700

    pe

--- database/seeds/UsersTableSeeder.php
+++ database/seeds/UsersTableSeeder.php
@@ -23,7 +23,7 @@ class UsersTableSeeder extends Seeder
         $user->save();
 
         //insert juga ke relasinya 
-        $pegawai = Pegawai::where('id', 3)->first();
+        $pegawai = Pegawai::where('id', 1)->first();
         $user->$pegawai()->associate($pegawai);
         $user->save();
 
