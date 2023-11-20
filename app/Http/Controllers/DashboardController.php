be491b72dd1f55d6f7d7369c28e35cfc094dbc7f
rijalllhd <ijal1445b@gmail.com>
2023-11-20 13:38:27 +0700

    hhh

--- app/Http/Controllers/DashboardController.php
+++ app/Http/Controllers/DashboardController.php
@@ -8,14 +8,8 @@ class DashboardController extends Controller
 {
     public function dashboard ()
     {
-         $title = 'Dashboard'; // Data tambahan yang ingin Anda kirim ke tampilan
-
+        $title = 'Dashboard'; // Data tambahan yang ingin Anda kirim ke tampilan
         return view('dashboard.index', compact('title'));
-    }
-
-    public function dashboardadmin ()
-    {
-        return view('dashboard.admin');
     }
 
     public function dashboardadmin ()
