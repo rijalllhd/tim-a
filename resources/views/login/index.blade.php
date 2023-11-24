34f20e7e317adbf0879afe24ece9c1e0a6150036
rijalllhd <ijal1445b@gmail.com>
2023-11-24 13:49:23 +0700

    benerin asset

--- resources/views/login/index.blade.php
+++ resources/views/login/index.blade.php
@@ -18,7 +18,7 @@
         rel="stylesheet">
 
     <!-- Custom styles for this template-->
-    <link href="css/sb-admin-2.min.css" rel="stylesheet">
+    <link href="{{ asset('asset/css/sb-admin-2.min.css') }}" rel="stylesheet">
     {{-- SweetAlert --}}
     <script src="https://cdn.jsdelivr.net/npm/sweetalert2@10"></script>
 
@@ -99,14 +99,14 @@
     </div>
 
     <!-- Bootstrap core JavaScript-->
-    <script src="vendor/jquery/jquery.min.js"></script>
-    <script src="vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
+    <script src='{{ asset("vendor/jquery/jquery.min.js")}}'></script>
+    <script src='{{ asset("vendor/bootstrap/js/bootstrap.bundle.min.js")}}'></script>
 
     <!-- Core plugin JavaScript-->
-    <script src="vendor/jquery-easing/jquery.easing.min.js"></script>
+    <script src='{{  asset("vendor/jquery-easing/jquery.easing.min.js")}}'></script>
 
     <!-- Custom scripts for all pages-->
-    <script src="js/sb-admin-2.min.js"></script>
+    <script src="{{ asset ('js/sb-admin-2.min.js') }}"></script>
 
 </body>
 
